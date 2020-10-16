769ed991848ced0d8078f58a26f7c6f2a9753d8b
Vimal Patel <94vimalpatel@gmail.com>
2020-10-16 14:17:37 +0530

    feat: create the dashboard route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import CustomNavbar from './CustomNavbar'
+import Dashboard from './Dashboard'
 
 function App() {
   return (
     <div className='App'>
       <CustomNavbar />
+
+      <Switch>
+        <Route exact path='/'>
+          <Dashboard />
+        </Route>
+      </Switch>
     </div>
   )
 }
---
+++ src/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+
+function Dashboard() {
+  return (
+    <div className='dashboard'>
+      <Container className='text-light'>
+        <h1 className='display-4 pt-5 mb-0'>Recall flashcards</h1>
+        <p className='lead w-75 mb-4'>
+          Create flashcards for free! Break down your study material into small
+          digestible bits of information. Learn until you reach mastery. No ads
+          whatsoever for full immersive learning.
+        </p>
+        <Button variant='danger'>Get Started</Button>
+      </Container>
+    </div>
+  )
+}
+
+export default Dashboard
