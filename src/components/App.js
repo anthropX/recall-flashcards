aae805d7d9899a37f16a5240dbc0bc2bb1072df1
Vimal Patel <94vimalpatel@gmail.com>
2020-10-24 14:19:42 +0530

    feat: change footer color based on is dark

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ const App = ({ isDark }) => {
         </Container>
       </div>
 
-      <Footer />
+      <Footer isDark={isDark} />
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Dashboard = () => {
+const Dashboard = ({ isDark }) => {
   return (
-    <footer className='position-absolute bg-transparent text-white vw-100 text-center'>
+    <footer
+      className={`position-absolute bg-transparent text-${
+        isDark ? 'light' : 'dark'
+      } vw-100 text-center`}>
       <p>Recall © All Rights Reserved</p>
     </footer>
   )
+}
+
+Dashboard.propTypes = {
+  isDark: PropTypes.bool.isRequired,
 }
 
 export default Dashboard
