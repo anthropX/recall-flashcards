a00753bc5a8eaf3ac17434357b5f5894d9e21768
Vimal Patel <94vimalpatel@gmail.com>
2020-10-28 21:27:07 +0530

    feat: remove deck focus style on blur
    Focus style must be removed only if the click is inside decks page,
    but not on a particular deck or on the sidebar

--- src/components/DecksPage.js
+++ src/components/DecksPage.js
@@ -3,8 +3,31 @@ import Decks from './Decks'
 import DecksInfoCard from './DecksInfoCard'
 
 const DecksPage = () => {
+  const blurDeckGraphics = (event) => {
+    console.log(
+      'blurring blurring',
+      event.target.tagName,
+      !document.querySelector('.fluid-overlay').contains(event.target),
+      event.target.className,
+      event.target.className.match('deck-graphic'),
+    )
+
+    if (
+      !document.querySelector('.fluid-overlay').contains(event.target) &&
+      !event.target.className.match(/deck-graphic/)
+    ) {
+      console.log('blurred blurred')
+      document
+        .querySelectorAll('.decks .deck-graphic')
+        .forEach((graphic) => graphic.classList.remove('deck-graphic--opened'))
+    }
+  }
+
   return (
-    <div className='decks-page d-flex flex-column flex-md-row'>
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
+    <div
+      className='decks-page d-flex flex-column flex-md-row'
+      onClick={blurDeckGraphics}>
       <div className='decks-main flex-grow-1 mr-auto pr-0 pr-md-5' sm='auto'>
         <h1 className='display-5 mb-1'>Decks</h1>
         <p className='p1 mb-0 pr-0 pr-md-5'>
