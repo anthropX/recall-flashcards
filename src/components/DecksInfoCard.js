27e7424dd8ea09cf8dd1dd19704cf5c0a9c656fa
Vimal Patel <94vimalpatel@gmail.com>
2020-10-28 18:23:51 +0530

    feat: add reset deck

--- src/components/DecksInfoCard.js
+++ src/components/DecksInfoCard.js
@@ -5,9 +5,17 @@ import Col from 'react-bootstrap/Col'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
 const DecksInfoCard = ({ deck: { name, desc, mastered, total } }) => {
+  const showReset = () =>
+    document.querySelector('.fluid-box').classList.add('fluid-box--reset')
+
+  const hideReset = () =>
+    document.querySelector('.fluid-box').classList.remove('fluid-box--reset')
+
   const closeSidebar = () => {
+    // Close Sidebar
     document
       .querySelector('.fluid-overlay')
       .classList.remove('fluid-overlay--overlaid')
@@ -15,6 +23,8 @@ const DecksInfoCard = ({ deck: { name, desc, mastered, total } }) => {
     document
       .querySelectorAll('.decks .deck-graphic')
       .forEach((graphic) => graphic.blur())
+    // Hide Reset, in case any
+    hideReset()
   }
 
   const handleSidebarClick = (event) => {
@@ -64,14 +74,38 @@ const DecksInfoCard = ({ deck: { name, desc, mastered, total } }) => {
               </Link>
             </Col>
             <Col xs='3' className='pl-0 pr-md-3'>
-              <a
-                href='/decks'
-                className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'>
+              <button
+                className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'
+                type='button'
+                onClick={showReset}>
                 <i className='aside__icon fas fa-sync-alt' />
                 <p className='p2 mt-2 mb-0 text-nowrap'>Reset Progress</p>
-              </a>
+              </button>
             </Col>
           </Row>
+        </aside>
+        <aside className='decks-infocard aside pl-md-3 pr-md-0 d-flex flex-column'>
+          <div className='aside__close'>
+            <i className='fas fa-times text-muted' />
+          </div>
+          <p className='p1'>
+            Are you sure you want to reset progress for the deck,{' '}
+            <strong>Periodic Table</strong>? This action cannot be undone
+          </p>
+          <Button
+            variant='outline-secondary'
+            type='submit'
+            className='px-4 mt-2 mb-3'
+            onClick={hideReset}>
+            Cancel
+          </Button>
+          <Button
+            variant='outline-danger'
+            type='submit'
+            className='px-4 mt-2 mb-3'
+            onClick={hideReset}>
+            Confirm
+          </Button>
         </aside>
       </div>
     </div>
