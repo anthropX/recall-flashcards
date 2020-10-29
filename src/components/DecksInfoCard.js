e3a751fd0489cd4f7338aa3f772f300eb6251eb5
Vimal Patel <94vimalpatel@gmail.com>
2020-10-29 12:31:25 +0530

    fix: remove left margin from lightbox hr

--- src/components/DecksInfoCard.js
+++ src/components/DecksInfoCard.js
@@ -71,7 +71,7 @@ const DecksInfoCard = ({ selectedDeck: { name, desc, mastered, total } }) => {
             srOnly={getProgressPercentage() < 6}
           />
           <p className='p1 mt-3 mb-0'>{desc}</p>
-          <hr className='hr mt-2 mb-4' />
+          <hr className='hr mt-2 mb-4 ml-0' />
           <Row>
             <Col xs='3' className='pl-2 pr-0'>
               <Link
