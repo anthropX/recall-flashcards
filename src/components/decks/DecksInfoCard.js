707eb13aaeb433ab16fab30535debd0bf853f28a
Vimal Patel <94vimalpatel@gmail.com>
2020-11-27 00:59:25 +0530

    fix: ensure decks don't take up full width when infocard is invisible

--- src/components/decks/DecksInfoCard.js
+++ src/components/decks/DecksInfoCard.js
@@ -15,6 +15,10 @@ const DecksInfoCard = ({
 }) => {
   const { name, desc, cards, buckets } = deckIndex !== -1 && decks[deckIndex]
   const [isResetVisible, setResetVisible] = useState(false)
+  let total
+  let mastered
+  if (cards) total = cards.length
+  if (buckets) mastered = buckets.mastered.length
 
   const closeSidebar = () => {
     setSidebarOverlaid(false)
@@ -28,8 +32,8 @@ const DecksInfoCard = ({
     closeSidebar()
 
   const getProgressPercentage = () => {
-    if (cards.length === 0) return 0
-    return Math.floor((buckets.mastered.length / cards.length) * 100)
+    if (total === 0) return 0
+    return Math.floor((mastered / total) * 100)
   }
 
   return (
@@ -46,53 +50,54 @@ const DecksInfoCard = ({
           isResetVisible && 'fluid-box--reset'
         } pl-md-3 pr-md-0`}
         tabIndex='-1'>
-        {typeof name !== 'undefined' ? (
-          <aside className='decks-infocard aside pl-md-3 pr-md-0'>
-            <h6 className='h6 mt-0'>{name}</h6>
-            <div className='aside__close'>
-              <i className='fas fa-times text-muted' />
-            </div>
-            <p className='p2 my-2 text-muted'>
-              Mastered {buckets.mastered.length} of {cards.length} cards
-            </p>
-            <ProgressBar
-              striped
-              variant='danger'
-              now={getProgressPercentage()}
-              label={`${getProgressPercentage()}%`}
-              srOnly={getProgressPercentage() < 6}
-            />
-            <p className='p1 mt-3 mb-0'>{desc}</p>
-            <hr className='hr mt-2 mb-4 ml-0' />
-            <Row>
-              <Col xs='3' className='pl-2 pr-0'>
-                <Link
-                  to={`/decks/${deckIndex}/play`}
-                  className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'>
-                  <i className='aside__icon fas fa-play' />
-                  <p className='p2 mt-2 mb-0'>Play</p>
-                </Link>
-              </Col>
-              <Col xs='3' className='pl-0 pr-md-3'>
-                <Link
-                  to={`/decks/${deckIndex}`}
-                  className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'>
-                  <i className='aside__icon fas fa-cog' />
-                  <p className='p2 mt-2 mb-0'>Configure</p>
-                </Link>
-              </Col>
-              <Col xs='3' className='pl-0 pr-md-3'>
-                <button
-                  className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'
-                  type='button'
-                  onClick={() => setResetVisible(true)}>
-                  <i className='aside__icon fas fa-sync-alt' />
-                  <p className='p2 mt-2 mb-0 text-nowrap'>Reset Progress</p>
-                </button>
-              </Col>
-            </Row>
-          </aside>
-        ) : null}
+        <aside
+          className={`decks-infocard aside ${
+            typeof name === 'undefined' && 'invisible'
+          } pl-md-3 pr-md-0`}>
+          <h6 className='h6 mt-0'>{name}</h6>
+          <div className='aside__close'>
+            <i className='fas fa-times text-muted' />
+          </div>
+          <p className='p2 my-2 text-muted'>
+            Mastered {mastered} of {total} cards
+          </p>
+          <ProgressBar
+            striped
+            variant='danger'
+            now={getProgressPercentage()}
+            label={`${getProgressPercentage()}%`}
+            srOnly={getProgressPercentage() < 6}
+          />
+          <p className='p1 mt-3 mb-0'>{desc}</p>
+          <hr className='hr mt-2 mb-4 ml-0' />
+          <Row>
+            <Col xs='3' className='pl-2 pr-0'>
+              <Link
+                to={`/decks/${deckIndex}/play`}
+                className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'>
+                <i className='aside__icon fas fa-play' />
+                <p className='p2 mt-2 mb-0'>Play</p>
+              </Link>
+            </Col>
+            <Col xs='3' className='pl-0 pr-md-3'>
+              <Link
+                to={`/decks/${deckIndex}`}
+                className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'>
+                <i className='aside__icon fas fa-cog' />
+                <p className='p2 mt-2 mb-0'>Configure</p>
+              </Link>
+            </Col>
+            <Col xs='3' className='pl-0 pr-md-3'>
+              <button
+                className='aside__option text-decoration-none d-flex flex-column align-items-center p-2'
+                type='button'
+                onClick={() => setResetVisible(true)}>
+                <i className='aside__icon fas fa-sync-alt' />
+                <p className='p2 mt-2 mb-0 text-nowrap'>Reset Progress</p>
+              </button>
+            </Col>
+          </Row>
+        </aside>
         <aside className='decks-infocard aside pl-md-3 pr-md-0 d-flex flex-column'>
           <div className='aside__close'>
             <i className='fas fa-times text-muted' />
