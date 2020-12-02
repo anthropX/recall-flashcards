3545dc38f197ecb89d4e065ebe6968c7010aeb0f
Vimal Patel <94vimalpatel@gmail.com>
2020-12-02 20:14:16 +0530

    fix: make sure 404 text scrolls with the page
    Without the fix, the 404 text remained fixed at the center
    irrespective of page scroll on the phone

--- src/components/layout/NotFound.js
+++ src/components/layout/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='not-found position-fixed d-flex justify-content-center align-items-center'>
+    <div className='not-found d-flex justify-content-center align-items-center'>
       <div className='not-found-message position-relative d-flex align-items-center'>
         <h1 className='display-5 display-5-0 text-danger m-0 mt-0'>404</h1>
         <div className='vertical-line bg-danger mx-3' />
